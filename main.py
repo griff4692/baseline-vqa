741c834ad7071b6c4f8f583b9781a05fcf217cc0
Griffin Adams <griff4692@gmail.com>
2017-02-15 15:17:08 -0500

    Adds data dir and main.py to pre-process data.

---
+++ main.py
@@ -0,0 +1,33 @@
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy as np
+import json
+from collections import Counter
+
+# open data file and parse
+dataFileR = open('./data/data.json', 'r')
+datasetJSON = dataFileR.read()
+dataset =  json.loads(datasetJSON)
+dataFileR.close()
+
+# Input: arrow of answers
+# takes majority vote
+def majorityAnswer(answers):
+	answer, count = Counter(answers).most_common(1)[0]
+	return answer
+
+# open file for writing processed training examples
+dataFileW = open('./data/data_processed.csv', 'w')
+
+for imgId in dataset:
+	questions = dataset[imgId]
+
+	# each question counts as its own training example
+	for question in questions:
+		trainExampleStr = str(imgId) + ',' + str(question) + ',' + str(majorityAnswer(questions[question]))
+		dataFileW.write(trainExampleStr + '\n')
+
+print "finished processing data..."
+
+# close up shop
+dataFileW.close()
