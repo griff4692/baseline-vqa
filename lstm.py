a03e8f22a946b5b00905411f3094449a17ea4e38
soumyaw <wadhwa.soumya@gmail.com>
2017-02-19 12:18:37 -0500

    Add files via upload

--- lstm.py
+++ lstm.py
@@ -1,16 +1,79 @@
+import numpy
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+from keras.callbacks import ModelCheckpoint
 import json
+import word_table as w_t
+from keras.utils import np_utils
 
-finq = open("Questions_Train_mscoco/OpenEnded_mscoco_train2014_questions.json", 'r')
-fina = open("mscoco_train2014_annotations.json", 'r')
-fout = open("open_ques_ans.json", 'w')
+wt = w_t.WordTable()
+wt.load_dictionary()
+wt.top_answers('data/data_processed_val.csv')
 
-questions = json.load(finq)
-answers = json.load(fina)
+top_words = len(wt.word2Idx)+1
+import csv
+import numpy as np
+ques_maxlen = 20
+X = []
+Y = []
+#mapping = {}
+total = 0
+with open('data/data_processed.csv', 'rb') as csvfile:
+    dp = csv.reader(csvfile, delimiter='~')
+    for row in dp:
+        ques = row[1]
+        ques = ques.lower().strip().strip('?!.').split()
+        x = np.zeros(ques_maxlen)
+        leng = len(ques)
+        try:
+		Y.append(wt.labels2Idx[row[2]])
+		for i in range(ques_maxlen):
+            		if i < leng:
+                		x[i] = wt.getIdx(ques[i])+1
+		X.append(x)
+	except:
+		pass
 
-for ques in questions['questions']:
-	print ques
-	break
-	
-for ans in answers['annotations']:
-	print ans
-	break
+X_data = np.array(X)
+#X_data = np.reshape(X_data, (X_data.shape[0], X_data.shape[1], 1))
+Y_data = np_utils.to_categorical(Y)
+
+X = []
+Y = []
+
+with open('data/data_processed_val.csv', 'rb') as csvfile:
+    dp = csv.reader(csvfile, delimiter='~')
+    for row in dp:
+        ques = row[1]
+        ques = ques.lower().strip().strip('?!.').split()
+        x = np.zeros(ques_maxlen)
+        leng = len(ques)
+        try:
+		Y.append(wt.labels2Idx[row[2]])
+		for i in range(ques_maxlen):
+            		if i < leng:
+                		x[i] = wt.getIdx(ques[i])+1
+		X.append(x)
+	except:
+		pass
+
+X_test = np.array(X)
+#X_data = np.reshape(X_data, (X_data.shape[0], X_data.shape[1], 1))
+Y_test = np_utils.to_categorical(Y)
+    
+# create the model
+embedding_vecor_length = 512
+model = Sequential()
+model.add(Embedding(top_words, embedding_vecor_length, input_length=ques_maxlen))
+model.add(LSTM(300, dropout_W = 0.2, dropout_U = 0.2))
+model.add(Dense(Y_data.shape[1], activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model.summary())
+
+model.fit(X_data, Y_data, nb_epoch=10, batch_size = 64)
+# Final evaluation of the model
+scores, acc = model.evaluate(X_test, Y_test, verbose=1)
+print acc
